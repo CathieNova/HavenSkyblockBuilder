6c87a30a5049adcfdfb30d0308e6e06d0c6ff58d
Cyclic <cyclic007dev@gmail.com>
2025-01-19 17:21:13 -0600

    going crazy trying to setup gui

---
+++ src/main/java/net/cathienova/haven_skyblock_builder/Gui/IslandScreen.java
@@ -0,0 +1,21 @@
+package net.cathienova.haven_skyblock_builder.Gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.network.chat.Component;
+
+public class IslandScreen extends Screen {
+
+    protected IslandScreen(Component title) {
+        super(title);
+    }
+    @Override
+    protected void init() {
+        super.init();
+
+    }
+}
