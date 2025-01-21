f2a138ddb866c5da0c52304bf0a82370a20ac252
Cathienna <Cathienna69@gmail.com>
2025-01-21 22:20:51 +0100

    Added base gui screen

--- src/main/java/net/cathienova/haven_skyblock_builder/Gui/IslandScreen.java
+++ src/main/java/net/cathienova/haven_skyblock_builder/Gui/IslandScreen.java
@@ -1,21 +1,75 @@
 package net.cathienova.haven_skyblock_builder.Gui;
 
+import net.cathienova.haven_skyblock_builder.HavenSkyblockBuilder;
+import net.cathienova.haven_skyblock_builder.team.Team;
+import net.cathienova.haven_skyblock_builder.team.TeamManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.StringWidget;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
+
+import java.util.Collection;
 
 public class IslandScreen extends Screen {
 
-    protected IslandScreen(Component title) {
-        super(title);
+    public IslandScreen() {
+        super(Component.translatable("haven_skyblock_builder.gui.screen"));
     }
+
     @Override
     protected void init() {
-        super.init();
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+        int spacing = 10;
 
+        Team currentTeam = TeamManager.getTeamByPlayer(Minecraft.getInstance().player.getUUID());
+
+        if (currentTeam == null) {
+            Component WIP = Component.literal("This is Work in Progress");
+            Font font = Minecraft.getInstance().font;
+            StringWidget WIPLabel = new StringWidget(WIP, font);
+            WIPLabel.setX(centerX - font.width(WIP.getString()) / 2);
+            WIPLabel.setY(centerY);
+            this.addRenderableWidget(WIPLabel);
+        } else {
+            Component WIP = Component.literal("This is Work in Progress");
+            Font font = Minecraft.getInstance().font;
+            StringWidget WIPLabel = new StringWidget(WIP, font);
+            WIPLabel.setX(centerX - font.width(WIP.getString()) / 2);
+            WIPLabel.setY(centerY - spacing);
+            this.addRenderableWidget(WIPLabel);
+
+            Collection<Team> allTeams = TeamManager.getAllTeams();
+            int teamY = centerY;
+
+            for (Team team : allTeams) {
+                Component teamName = Component.literal(team.getName());
+                StringWidget teamLabel = new StringWidget(teamName, font);
+                teamLabel.setX(centerX - font.width(teamName.getString()) / 2);
+                teamLabel.setY(teamY);
+                this.addRenderableWidget(teamLabel);
+                teamY += spacing;
+            }
+        }
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        Minecraft.getInstance().getTextureManager().bindForSetup(HavenSkyblockBuilder.loc("textures/gui/hsb_gui.png"));
+        int centerX = (this.width - 176) / 2;
+        int centerY = (this.height - 166) / 2;
+        guiGraphics.blit(
+                HavenSkyblockBuilder.loc("textures/gui/hsb_gui.png"),
+                centerX, centerY,
+                0, 0,
+                176, 166,
+                256, 256
+        );
+        this.renderBackground(guiGraphics, mouseX, mouseY, partialTicks);
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
     }
 }
--- src/main/java/net/cathienova/haven_skyblock_builder/datagen/ModEngLangProvider.java
+++ src/main/java/net/cathienova/haven_skyblock_builder/datagen/ModEngLangProvider.java
@@ -83,6 +83,11 @@ public class ModEngLangProvider extends LanguageProvider
 
         add("haven_skyblock_builder.reload", prefix + "§aConfig reloaded.");
 
+        add("haven_skyblock_builder.gui.screen", "Haven Skyblock Builder");
+
+        add("key.categories.haven_skyblock_builder", "Haven Skyblock Builder");
+        add("key.haven_skyblock_builder.open_skyblock_screen", "Open Haven Skyblock Builder GUI");
+
         add("generator.haven_skyblock_builder.skyblock_world", "Haven Skyblock");
         add("haven_skyblock_builder.message.skyblock_spawn", prefix + "§aYou don't have an island yet, you can make one with '/havensb island create <template> <name>'.");
     }
